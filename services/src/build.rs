1d50694dcb854f6c9a8bf29af1107a8512716941
al amoda <alamoda@tutamail.com>
2025-01-31 08:11:58 -0500

    patch path in build.rs

--- services/src/build.rs
+++ services/src/build.rs
@@ -2,7 +2,8 @@ use std::{env, ffi::OsString};
 mod fetcher;
 pub fn main() {
     let manifest_dir = std::path::PathBuf::from(
-        &(env::var("CARGO_MANIFEST_DIR").expect("cargo manifest dir to unwrap")),
+        //&(env::var("CARGO_MANIFEST_DIR").expect("cargo manifest dir to unwrap")),
+        env::current_dir().expect("env current dir to unwrap"),
     );
     let path_thingy = manifest_dir
         .join("fetched_resourcezzz")
